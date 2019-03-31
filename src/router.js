d0d2ebbad70fba8db1e61e68a4ddc2e9f2efb499
ygn <947498533@qq.com>
2019-03-31 23:15:31 +0800

    因为开放 API 接口的问题，这里路由有所改动，以方便项目进行因为开放 API 接口的问题，这里路由有所改动，以方便项目进行

--- src/router.js
+++ src/router.js
@@ -1,10 +1,26 @@
-import VueRouter from 'vue-router'
+import VueRouter from 'vue-router';
+
+import HomeContainer from './components/tabbar/HomeContainer.vue'
+import MemberContainer from './components/tabbar/MemberContainer.vue'
+import ShopcarContainer from './components/tabbar/ShopcarContainer.vue'
+import SearchContainer from './components/tabbar/SearchContainer.vue'
+import NewsList from './components/news/NewsList.vue'
+import NewsInfo from './components/news/NewsInfo.vue'
 
 
 // 3. 创建路由对象
 var router = new VueRouter({
-  routes: [
-  ]
+  routes: [ // 配置路由规则
+    { path: '/', redirect: '/home' },
+    { path: '/home', component: HomeContainer},
+    { path: '/member', component: MemberContainer },
+    { path: '/shopcar', component: ShopcarContainer },
+    { path: '/search', component: SearchContainer },
+    { path: '/home/newslist', component: NewsList},
+    // { path: '/home/newsinfo/:id', component: NewsInfo } 因为开放 API 接口的问题，这里路由有所改动，以方便项目进行
+    { path: '/home/newsinfo/', component: NewsInfo }
+  ],
+  linkActiveClass: 'mui-active' // 覆盖默认的路由高亮的类，默认的类叫做 router-link-active
 })
 
 // 把路由对象暴露出去
